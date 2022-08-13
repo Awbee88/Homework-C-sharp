886ef1f82b8c4dbc7291244118918f94788d9d04
ilyaLyapin <eximo03@gmail.com>
2022-08-14 00:26:58 +0300

    Homework 9 is done

---
+++ Sem9 homework/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+/*
+
+int SumOfElements(int m, int n)
+{
+    if(m==n)
+    {
+        return m;
+    }
+    else
+    {
+        return m + SumOfElements(m+1,n);
+    }
+    
+}
+
+Console.Write(SumOfElements(1,15));
+
+*/
+
+
+
+// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+
+int SumOfNums(int num)
+{
+    if(num/10==0)
+    {
+        return num;
+    }
+    else
+    {
+        return num%10 + SumOfNums(num/10);
+    }
+}
+
+Console.Write(SumOfNums(4533));
